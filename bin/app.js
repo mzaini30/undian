2a0a8ef6d2836eacf34d83a01862e70551acd385
mzaini30 <muhzaini30@gmail.com>
2017-05-09 10:01:48 +0800

    hai

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,32 @@
 $(function() {
+
     $(".proses").click(function() {
-        localStorage.setItem("undian", $(".data-undian").val())
-        alert(localStorage.getItem("undian"))
+        localStorage.setItem("undian", $(".data-undian").val().split("\n"))
+        location.href = "proses.html"
     })
+
+    $(".kembali").hide()
+    arrayundian = localStorage.getItem("undian").split(",")
+    $(".hasil").html(arrayundian[Math.floor(Math.random() * arrayundian.length)])
+    $(".hasil").hide()
+    x = 0
+    setInterval(function() {
+        $(".acak").html(arrayundian[x])
+        x++
+        if (x >= arrayundian.length) {
+            x = 0
+        }
+    }, 100)
+
+    $(".berhenti").click(function() {
+        $(".acak").hide()
+        $(".hasil").show()
+        $(".berhenti").hide()
+        $(".kembali").show()
+    })
+
+    $(".kembali").click(function() {
+        location.href = "index.html"
+    })
+
 })
