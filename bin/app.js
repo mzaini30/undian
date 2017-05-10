85c0863699fcec12290973388cc5edf62e171dae
mzaini30 <muhzaini30@gmail.com>
2017-05-11 07:46:32 +0800

    hai

--- bin/app.js
+++ bin/app.js
@@ -1,16 +1,38 @@
 $(function() {
 
+    function shuffle(array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
     $(".proses").click(function() {
         localStorage.setItem("undian", $(".data-undian").val().split("\n"))
         location.href = "proses.html"
     })
 
     arrayundian = localStorage.getItem("undian").split(",")
+    acakarrayundian = shuffle(arrayundian)
+
     $(".hasil").html(arrayundian[Math.floor(Math.random() * arrayundian.length)])
     $(".hasil").hide()
     x = 0
     setInterval(function() {
-        $(".acak").html(arrayundian[x])
+        $(".acak").html(acakarrayundian[x])
         x++
         if (x >= arrayundian.length) {
             x = 0
