95d2f6b76b61b0fab06f3ef834f329904087150d
mzaini30 <muhzaini30@gmail.com>
2018-05-14 13:55:38 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -4,52 +4,54 @@ $(function() {
 
     function shuffle(array) {
         var currentIndex = array.length,
-            temporaryValue, randomIndex;
+            temporaryValue, randomIndex
 
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
 
             // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
 
             // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
+            temporaryValue = array[currentIndex]
+            array[currentIndex] = array[randomIndex]
+            array[randomIndex] = temporaryValue
         }
 
-        return array;
+        return array
     }
 
     $(".proses").click(function() {
-        localStorage.setItem("undian", $(".data-undian").val().split("\n"));
-        location.href = "proses.html";
-    });
+        localStorage.setItem("undian", $(".data-undian").val().split("\n"))
+        location.href = "proses.html"
+    })
 
-    arrayundian = localStorage.getItem("undian").split(",");
-    arrayundian = arrayundian.filter(entry => entry.trim() != '');
-    acakarrayundian = shuffle(arrayundian);
+    arrayundian = localStorage.getItem("undian").split(",")
+    arrayundian = arrayundian.filter(function(entry){
+        return entry.trim() != ''
+    })
+    acakarrayundian = shuffle(arrayundian)
 
-    $(".hasil").html(arrayundian[Math.floor(Math.random() * arrayundian.length)]);
-    $(".hasil").hide();
-    x = 0;
+    $(".hasil").html(arrayundian[Math.floor(Math.random() * arrayundian.length)])
+    $(".hasil").hide()
+    x = 0
     setInterval(function() {
-        $(".acak").html(acakarrayundian[x]);
-        x++;
+        $(".acak").html(acakarrayundian[x])
+        x++
         if (x >= arrayundian.length) {
-            x = 0;
+            x = 0
         }
-    }, 50);
+    }, 50)
 
     $(".berhenti").click(function() {
-        $(".acak").hide();
-        $(".hasil").show();
-        $(".berhenti").hide();
-    });
+        $(".acak").hide()
+        $(".hasil").show()
+        $(".berhenti").hide()
+    })
 
     $(".kembali").click(function() {
-        location.href = "index.html";
+        location.href = "index.html"
     })
 
 })
